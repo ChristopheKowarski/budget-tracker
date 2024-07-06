3f68588349e7118777d345a9992040fe2224ee78
ChristopheKowarski <christophe.kowarski@gmail.com>
2024-07-06 09:46:29 +0930

    added 'key' on outer div of loop (lines 96....)

--- app/(dashboard)/_components/CategoriesStats.tsx
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -93,7 +93,7 @@ function CategoriesCard({
                   const amount = item._sum.amount || 0;
                   const percentage = (amount * 100) / (total || amount);
                   return (
-                    <div className="flex flex-col gap-2">
+                    <div key={amount} className="flex flex-col gap-2">
                       <div className="flex items-center justify-between">
                         <span className="flex items-center text-gray-400">
                           {item.categoryIcon} {item.category}
